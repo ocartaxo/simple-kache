c279ae65b7653b52fc49667644081faf50b552b3
Otavio Cartaxo <oacartaxo@gmail.com>
2025-11-19 13:36:33 -0300

    mutable map

--- src/main/kotlin/impl/PerpetualKache.kt
+++ src/main/kotlin/impl/PerpetualKache.kt
@@ -7,7 +7,7 @@ import br.com.ocartaxo.GenericKache
  * [PerpetualKache] caches the items perpetually unless they're manually [remove]ed.
  */
 class PerpetualKache<K, V> : GenericKache<K, V> {
-    private val cache = hashMapOf<K, V>()
+    private val cache = mutableMapOf<K, V>()
 
     override val size: Int
         get() = cache.size

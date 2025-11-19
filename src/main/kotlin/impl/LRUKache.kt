2b7256948db2c5d4f447c96442c6b9232ac95ba3
Otavio Cartaxo <oacartaxo@gmail.com>
2025-11-19 12:55:18 -0300

    LRU cache strategy

---
+++ src/main/kotlin/impl/LRUKache.kt
@@ -0,0 +1,47 @@
+package br.com.ocartaxo.impl
+
+import br.com.ocartaxo.GenericKache
+
+/**
+ * [LRUKache] flushes items that are **Least Recently Used** and keeps [minimalSize] items at most.
+ */
+class LRUKache<K, V>(
+    private val delegate: GenericKache<K, V>,
+    private val minimalSize: Int = DEFAULT_SIZE
+) : GenericKache<K,V> by delegate {
+    private val keyMap = object : LinkedHashMap<K, Boolean>(minimalSize, .75f, true) {
+        override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, Boolean>): Boolean {
+            val tooManyCachedItems = size > minimalSize
+            if (tooManyCachedItems) eldestKeyToRemove = eldest.key
+            return tooManyCachedItems
+        }
+    }
+
+    private var eldestKeyToRemove: K? = null
+
+    override fun set(key: K, value: V) {
+        delegate[key] = value
+        cycleKeyMap(key)
+    }
+
+    override fun get(key: K): V? {
+        keyMap[key]
+        return delegate[key]
+    }
+
+    override fun clear() {
+        keyMap.clear()
+        delegate.clear()
+    }
+
+    private fun cycleKeyMap(key: K) {
+        keyMap[key] = PRESENT
+        eldestKeyToRemove?.let { delegate.remove(it) }
+        eldestKeyToRemove = null
+    }
+
+    companion object {
+        private const val DEFAULT_SIZE = 100
+        private const val PRESENT = true
+    }
+}

a6751d26d6f42dc12b02ca9cc4c4a23a315f9a91
Otavio Cartaxo <oacartaxo@gmail.com>
2025-11-19 13:49:38 -0300

    memory-sensitive cache strategy

---
+++ src/main/kotlin/impl/SoftKache.kt
@@ -0,0 +1,47 @@
+package br.com.ocartaxo.impl
+
+import br.com.ocartaxo.Kache
+import java.lang.ref.ReferenceQueue
+import java.lang.ref.SoftReference
+
+/**
+ * [SoftKache] caches items with a [SoftReference] wrapper.
+ */
+class SoftKache(
+    private val delegate: Kache
+) : Kache by delegate {
+    private val referenceQueue = ReferenceQueue<Any>()
+
+    private class SoftEntry(
+        val key: Any,
+        value: Any,
+        referenceQueue: ReferenceQueue<Any>
+    ) : SoftReference<Any>(value, referenceQueue)
+
+    override fun set(key: Any, value: Any) {
+        removeUnreachableItems()
+        val softEntry = SoftEntry(key, value, referenceQueue)
+        delegate[key] = softEntry
+    }
+
+    override fun remove(key: Any) {
+        delegate.remove(key)
+        removeUnreachableItems()
+    }
+
+    override fun get(key: Any): Any? {
+        val softEntry = delegate[key] as SoftEntry?
+        softEntry?.get()?.let { return it }
+        delegate.remove(key)
+        return null
+    }
+
+    private fun removeUnreachableItems() {
+        var softEntry = referenceQueue.poll() as SoftEntry?
+        while (softEntry != null) {
+            val key = softEntry.key
+            delegate.remove(key)
+            softEntry = referenceQueue.poll() as SoftEntry?
+        }
+    }
+}
